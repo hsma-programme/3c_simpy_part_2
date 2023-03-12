962175a24be72f8d6afe6864bac85c17f182680f
wrongways skidmark <jez@wain.name>
2023-03-12 16:56:37 +0000

    Major refactor applying SW engineering principles
    Removed all the repeated code in the patient_journey method by extracting the wait for resource and the wait for service time into a separate method
    
    Removed a bug in Patient class by setting the ACU/ED choice in the init. As it was written, using the method, determine_acu_destiny - there’s a high chance (~30%) of getting a different value, which is obviously incorrect.
    
    Also corrected the line: 
    if patient.acu_patient == True
    
    acu_patient is already a boolean, all that’s required is:
    
    if patient.acu_patient:
    
    I renamed the attribute to make this clearer, so it’s now:
    
    if patient.is_acu_patient:
    
    Re-wrote the statistics collection. Appending pandas dataframes is a no-no because it involves massive data copying. Not a problem here with a small data set, but not a practice to be encouraged. Instead keep a simple python list holding dictionaries of the different service wait times for every patient. Once the trial is over, convert this to a pandas data frame and write to CSV
    
    I changed the calculation from mean to 90th percentile, which is more pertinent when looking at queues.
    
    Used python to automate initialisation, notably with comprehensions
    
    Used f-strings to tidy up the reports

--- 3C_SimPy_for_Discrete_Event_Simulation_Part_2/Solutions/exercise_1_solution.py
+++ 3C_SimPy_for_Discrete_Event_Simulation_Part_2/Solutions/exercise_1_solution.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import simpy
+from pathlib import Path
+from itertools import count
 import random
+
+import simpy
 import pandas as pd
-import csv
+
+RESULTS_CSV = "trial_ed_results.csv"
+SERVICES = ["Registration", "Triage", "ED_Assessment", "ACU_Assessment"]
+PERCENTILE = 90
+
 
 # Class to store global parameter values.  We don't create an instance of this
 # class - we just refer to the class blueprint itself to access the numbers
@@ -15,249 +22,116 @@ class g:
     mean_triage = 5
     mean_ed_assess = 30
     mean_acu_assess = 60
-    
+
     prob_acu = 0.2
-    
+
     number_of_receptionists = 1
     number_of_nurses = 2
     number_of_ed_doctors = 2
     number_of_acu_doctors = 1
-    
+
     sim_duration = 2880
     warm_up_duration = 1440
     number_of_runs = 100
-    
+
+
 # Class representing our patients coming in to the ED.  Here, we'll store a
 # patient ID and whether the patient will be sent to the ACU or stay in the
 # ED, along with a method that makes that determination randomly
 class ED_Patient:
     def __init__(self, p_id, prob_acu):
         self.id = p_id
-        self.prob_acu = prob_acu
-        self.acu_patient = False
-        
-        self.q_time_reg = 0
-        self.q_time_triage = 0
-        self.q_time_ed_assess = 0
-        self.q_time_acu_assess = 0
-        
-    # Method to determine whether or not this patient will be diverted to the
-    # ACU, based on their probability of being an ACU patient
-    def determine_acu_destiny(self):
-        if random.uniform(0, 1) < self.prob_acu:
-            self.acu_patient = True
-            
+        self.is_acu_patient = random.uniform(0, 1) < prob_acu
+        self.queue_times = {"P_ID": p_id, "ED_Assessment": None, "ACU_Assessment": None}
+
+
 # Class representing our model of the ED
 class ED_Model:
-    def __init__(self, run_number):
+    def __init__(self, run_number, csv_filename):
         self.env = simpy.Environment()
-        self.patient_counter = 0
-        
-        self.receptionist = simpy.Resource(self.env,
-                                           capacity=g.number_of_receptionists)
-        self.nurse = simpy.Resource(self.env,
-                                    capacity=g.number_of_nurses)
-        self.ed_doctor = simpy.Resource(self.env,
-                                        capacity=g.number_of_ed_doctors)
-        self.acu_doctor = simpy.Resource(self.env,
-                                         capacity=g.number_of_acu_doctors)
-        
+        staffing = [
+            g.number_of_receptionists, g.number_of_nurses,
+            g.number_of_ed_doctors, g.number_of_acu_doctors
+        ]
+        resources = [simpy.Resource(self.env, capacity=c) for c in staffing]
+        service_times = [g.mean_register, g.mean_triage, g.mean_ed_assess, g.mean_acu_assess]
+        self.resources = dict(zip(SERVICES, resources))
+        self.service_times = dict(zip(SERVICES, service_times))
+
         self.run_number = run_number
-        
-        self.mean_q_time_registration = 0
-        self.mean_q_time_triage = 0
-        self.mean_q_time_ed_assessment = 0
-        self.mean_q_time_acu_assessment = 0
-        
-        self.results_df = pd.DataFrame()
-        self.results_df["P_ID"] = []
-        self.results_df["Q_Time_Registration"] = []
-        self.results_df["Q_Time_Triage"] = []
-        self.results_df["Q_Time_ED_Assessment"] = []
-        self.results_df["Q_Time_ACU_Assessment"] = []
-        self.results_df.set_index("P_ID", inplace=True)
-        
+        self.results_csv_filename = csv_filename
+        self.queing_times = []
+
+    @property
+    def is_warming_up(self):
+        return self.env.now > g.warm_up_duration
+
     # A method that generates patients arriving at the ED
     def generate_ed_arrivals(self):
         # Keep generating indefinitely whilst the simulation is running
-        while True:
-            # Increment patient counter by 1
-            self.patient_counter += 1
-            
+        for patient_id in count(start=1):
             # Create a new patient
-            p = ED_Patient(self.patient_counter, g.prob_acu)
-            
-            # Determine the patient's ACU destiny by running the appropriate
-            # method
-            p.determine_acu_destiny()
-            
-            # Get the SimPy environment to run the ed_patient_journey method 
+            p = ED_Patient(patient_id, g.prob_acu)
+            p.queue_times["run_number"] = self.run_number
+            p.queue_times["ArrivalTime"] = self.env.now
+
+            # Get the SimPy environment to run the ed_patient_journey method
             # with this patient
             self.env.process(self.ed_patient_journey(p))
-            
+
             # Randomly sample the time to the next patient arriving
             sampled_interarrival = random.expovariate(1.0 / g.ed_inter)
-            
+
             # Freeze this function until that time has elapsed
             yield self.env.timeout(sampled_interarrival)
-            
-    def ed_patient_journey(self, patient):
-        """REGISTRATION"""
-        # Record the time the patient started queuing for registration
-        start_q_reg = self.env.now
-        
-        # Request a receptionist
-        with self.receptionist.request() as req:
-            # Freeze the function until the request can be met
-            yield req
-            
-            # Record the time the patient finished queuing for registration
-            end_q_reg = self.env.now
-            
-            # Calculate the time the patient was queuing and store in the
-            # patient's attribute
-            patient.q_time_reg = end_q_reg - start_q_reg
-            
-            # Randomly sample the time the patient will spend being registered
-            sampled_reg_duration = random.expovariate(1.0 / g.mean_register)
-            
-            # Freeze this function until that time has elapsed
-            yield self.env.timeout(sampled_reg_duration)
-            
-        """TRIAGE"""
-        # Record the time the patient started queuing for triage
-        start_q_triage = self.env.now
-        
-        # Request a nurse
-        with self.nurse.request() as req:
-            # Freeze the function until the request can be met
-            yield req
-            
-            # Record the time the patient finished queuing for triage
-            end_q_triage = self.env.now
-            
-            # Calculate the time the patient was queuing and store in the
-            # patient's attribute
-            patient.q_time_triage = end_q_triage - start_q_triage
-            
-            # Randomly sample the time the patient will spend being triaged
-            sampled_triage_duration = random.expovariate(1.0 / g.mean_triage)
-            
-            # Freeze this function until that time has elapsed
-            yield self.env.timeout(sampled_triage_duration)
-            
-        """BRANCH - ED ASSESSMENT OR ACU ASSESSMENT"""
-        # Check if patient destined for ACU or not, and either send to ACU
-        # for assessment, or keep in ED for assessment
-        if patient.acu_patient == True:
-            """ACU ASSESSMENT"""
-            # Record the time the patient started queuing for ACU assessment
-            start_q_acu_assess = self.env.now
-            
-            # Request an ACU doctor
-            with self.acu_doctor.request() as req:
-                # Freeze the function until the request can be met
-                yield req
-                
-                # Record the time the patient finished queuing for ACU 
-                # assessment
-                end_q_acu_assess = self.env.now
-                
-                # Calculate the time the patient was queuing and store in the
-                # patient's attribute
-                patient.q_time_acu_assess = (end_q_acu_assess - 
-                                             start_q_acu_assess)
-                
-                # Randomly sample the time the patient will spend being 
-                # assessed
-                sampled_acu_assess_duration = (
-                    random.expovariate(1.0 / g.mean_acu_assess))
-                
-                # Freeze this function until that time has elapsed
-                yield self.env.timeout(sampled_acu_assess_duration)
-        else:
-            """ED ASSESSMENT"""
-            # Record the time the patient started queuing for ED assessment
-            start_q_ed_assess = self.env.now
-            
-            # Request an ED doctor
-            with self.ed_doctor.request() as req:
-                # Freeze the function until the request can be met
-                yield req
-                
-                # Record the time the patient finished queuing for ED 
-                # assessment
-                end_q_ed_assess = self.env.now
-                
-                # Calculate the time the patient was queuing and store in the
-                # patient's attribute
-                patient.q_time_ed_assess = end_q_ed_assess - start_q_ed_assess
-                
-                # Randomly sample the time the patient will spend being 
-                # assessed
-                sampled_ed_assess_duration = (
-                    random.expovariate(1.0 / g.mean_ed_assess))
-                
-                # Freeze this function until that time has elapsed
-                yield self.env.timeout(sampled_ed_assess_duration)
-        
-        # If the warm up time has passed, then call the store_patient_results 
-        # method (this doesn't need to be processed by the environment, as it's
-        # not a generator function, just a conventional function)
-        if self.env.now > g.warm_up_duration:
-            self.store_patient_results(patient)
-        
-    # A method to store the patient's results (queuing times here) for this
-    # run alongside their patient ID in the Pandas DataFrame of the ED_Model
-    # class
-    def store_patient_results(self, patient):        
-        # First, because we have a branching path, this patient will have
-        # queued for either ED assessment or ACU assessment, but not both.
-        # Therefore, we need to check which happened, and insert NaNs
-        # (Not A Number) in the entries for the other queue in the DataFrame.
-        # NaNs are automatically ignored by Pandas when calculating the mean
-        # etc.  We can create a nan by casting the string 'nan' as a float :
-        # float("nan")
-        if patient.acu_patient == True:
-            patient.q_time_ed_assess = float("nan")
-        else:
-            patient.q_time_acu_assess = float("nan")
-            
-        df_to_add = pd.DataFrame({"P_ID":[patient.id],
-                                  "Q_Time_Registration":[patient.q_time_reg],
-                                  "Q_Time_Triage":[patient.q_time_triage],
-                                  "Q_Time_ED_Assessment":(
-                                      [patient.q_time_ed_assess]),
-                                  "Q_Time_ACU_Assessment":(
-                                      [patient.q_time_acu_assess]),
-                                  })
-        
-        df_to_add.set_index("P_ID", inplace=True)
-        self.results_df = self.results_df.append(df_to_add)
 
-    # A method that calculates the average queuing times for each queue.  We
-    # can call this at the end of each run
-    def calculate_mean_q_times(self):
-        self.mean_q_time_registration = (
-            self.results_df["Q_Time_Registration"].mean())
-        self.mean_q_time_triage = (
-            self.results_df["Q_Time_Triage"].mean())
-        self.mean_q_time_ed_assessment = (
-            self.results_df["Q_Time_ED_Assessment"].mean())
-        self.mean_q_time_acu_assessment = (
-            self.results_df["Q_Time_ACU_Assessment"].mean())
-        
-    # A method to write run results to file
-    def write_run_results(self):
-        with open("trial_ed_results.csv", "a") as f:
-            writer = csv.writer(f, delimiter=",")
-            results_to_write = [self.run_number,
-                                self.mean_q_time_registration,
-                                self.mean_q_time_triage,
-                                self.mean_q_time_ed_assessment,
-                                self.mean_q_time_acu_assessment]
-            writer.writerow(results_to_write)
-            
+    def do_service(self, service, patient):
+        """
+        Generic service call:
+        Waits for the service resource to be available
+        Then waits for the service to complete
+        The patient logs the time waiting in line
+        """
+
+        start_q_reg = self.env.now
+        resource = self.resources[service]
+        with resource.request() as req:
+            # Wait for available staff
+            yield req
+
+            q_time = self.env.now - start_q_reg
+            patient.queue_times[service] = q_time
+
+            # Sample the duration of the service and wait until then
+            mean_service_time = self.service_times[service]
+            sampled_reg_duration = random.expovariate(1.0 / mean_service_time)
+            yield self.env.timeout(sampled_reg_duration)
+
+    def ed_patient_journey(self, patient):
+        """Send our patient on her/his merry way..."""
+
+        treatment = "ACU_Assessment" if patient.is_acu_patient else "ED_Assessment"
+        process_services = [*SERVICES[:2], treatment]
+        for service in process_services:
+            yield from self.do_service(service, patient)
+            patient.queue_times["ExitTime"] = self.env.now
+
+        if not self.is_warming_up:
+            self.queing_times.append(patient.queue_times)
+
+    def store_results(self):
+        """
+        Store every patient's results (queuing times) for this run
+        along with their patient ID to a CSV file
+        """
+
+        df = pd.DataFrame(self.queing_times)
+        csv_path = Path(self.results_csv_filename)
+        if csv_path.exists():
+            df.to_csv(csv_path, mode="a", header=False, index=False)
+        else:
+            df.to_csv(csv_path, header=True, index=False)
+
     # The run method starts up the entity generators, and tells SimPy to start
     # running the environment for the duration specified in the g class. After
     # the simulation has run, it calls the methods that calculate run
@@ -265,71 +139,47 @@ class ED_Model:
     def run(self):
         # Start entity generators
         self.env.process(self.generate_ed_arrivals())
-        
-        # Run simulation
-        self.env.run(until=(g.sim_duration + g.warm_up_duration))
-        
-        # Calculate run results
-        self.calculate_mean_q_times()
-        
-        # Write run results to file
-        self.write_run_results()
 
-# Class to store, calculate and manipulate trial results
+        # Run simulation
+        self.env.run(until=g.sim_duration + g.warm_up_duration)
+
+        # Write run results to file
+        self.store_results()
+
+
 class Trial_Results_Calculator:
-    def __init__(self):
-        self.trial_results_df = pd.DataFrame()
-        
+    """Class to store, calculate and manipulate trial results"""
+
+    def __init__(self, results_csv_filename):
+        self.trial_results_df = pd.read_csv(results_csv_filename)
+
     # A method to read in the trial results and print them for the user
     def print_trial_results(self):
-        print ("TRIAL RESULTS")
-        print ("-------------")
-        
-        # Read in results from each run
-        self.trial_results_df = pd.read_csv("trial_ed_results.csv")
-        
+        title = "TRIAL RESULTS"
+        print(title)
+        print("-" * len(title))
+
         # Take average over runs
-        trial_mean_q_time_registration = (
-            self.trial_results_df["Mean_Q_Time_Registration"].mean())
-        trial_mean_q_time_triage = (
-            self.trial_results_df["Mean_Q_Time_Triage"].mean())
-        trial_mean_q_time_ed_assess = (
-            self.trial_results_df["Mean_Q_Time_ED_Assessment"].mean())
-        trial_mean_q_time_acu_assess = (
-            self.trial_results_df["Mean_Q_Time_ACU_Assessment"].mean())
-        
-        print ("Mean Queuing Time for Registration over Trial :",
-               f"{trial_mean_q_time_registration:.2f}")
-        print ("Mean Queuing Time for Triage over Trial :",
-               f"{trial_mean_q_time_triage:.2f}")
-        print ("Mean Queuing Time for ED Assessment over Trial :",
-               f"{trial_mean_q_time_ed_assess:.2f}")
-        print ("Mean Queuing Time for ACU Assessment over Trial :",
-               f"{trial_mean_q_time_acu_assess:.2f}")
+        for service in SERVICES:
+            text = f"{PERCENTILE}th percentile queue time for {service} over trial: "
+            q = PERCENTILE / 100
+            service_mean = self.trial_results_df[service].quantile(q)
+            print(f"{text:>58}{service_mean:5.1f} minutes")
 
-# Everything above is definition of classes and functions, but here's where
-# the code will start actively doing things.        
 
-# Create a file to store trial results
-with open("trial_ed_results.csv", "w") as f:
-    writer = csv.writer(f, delimiter=",")
-    column_headers = ["Run",
-                      "Mean_Q_Time_Registration",
-                      "Mean_Q_Time_Triage",
-                      "Mean_Q_Time_ED_Assessment",
-                      "Mean_Q_Time_ACU_Assessment"]
-    writer.writerow(column_headers)
 
 # For the number of runs specified in the g class, create an instance of the
 # ED_Model class, and call its run method
-for run in range(g.number_of_runs):
-    print (f"Run {run+1} of {g.number_of_runs}")
-    my_ed_model = ED_Model(run)
-    my_ed_model.run()
-    print ()
+def main():
+    for run in range(g.number_of_runs):
+        print(f"Run {run+1:03d} of {g.number_of_runs}", end="\r", flush=True)
+        my_ed_model = ED_Model(run, RESULTS_CSV)
+        my_ed_model.run()
+    print("\n")
 
-# Once the trial is complete, we'll create an instance of the
-# Trial_Result_Calculator class and run the print_trial_results method
-my_trial_results_calculator = Trial_Results_Calculator()
-my_trial_results_calculator.print_trial_results()
+    # Once the trial is complete, we'll create an instance of the
+    # Trial_Result_Calculator class and run the print_trial_results method
+    Trial_Results_Calculator(RESULTS_CSV).print_trial_results()
 
+
+main()
